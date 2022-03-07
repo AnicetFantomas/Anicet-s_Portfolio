73b215235db7f5751e7c8df511f4f9bf5899b57b
AnicetFantomas <94958024+AnicetFantomas@users.noreply.github.com>
2022-03-07 13:53:10 +0100

    Update css and js files

--- main.js
+++ main.js
@@ -2,18 +2,21 @@ const hamburgerButton = document.querySelector(".nav-btn");
 const slider = document.querySelector(".x-nav");
 const display = document.querySelector(".desktop-nav");
 const listEl = document.querySelectorAll("ul li")
+const body = document.querySelector("body")
 
 
 hamburgerButton.addEventListener("click", (e) => {
     display.style.display = "block";
     hamburgerButton.style.display = "none";
     slider.style.display = "block";
+    body.style.overflow = "hidden"
 })
 
 slider.addEventListener("click", (e) => {
     display.style.display = "none";
     hamburgerButton.style.display = "block";
     slider.style.display = "none";
+    body.style.overflow = "scroll"
 })
 
 
@@ -22,6 +25,7 @@ listEl.forEach(
         clickForClose.addEventListener("click", (e) => {
             display.style.display = "none";
             hamburgerButton.style.display = "block";
+            body.style.overflow = "scroll"
         })
     }
 )
